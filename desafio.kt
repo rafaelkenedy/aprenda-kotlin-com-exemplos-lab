0def34ae222b50e89de0050b6108d02eb5e35323
Rafael <35613507+rafaelkenedy@users.noreply.github.com>
2023-09-04 23:10:27 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,58 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (inscritos.contains(usuario)) {
+            println("O usuário ${usuario.nome} já está matriculado.")
+        } else {
+            inscritos.add(usuario)
+            println("O usuário ${usuario.nome} foi matriculado com sucesso.")
+        }
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo ${conteudo.nome} foi adicionado.")
+    }
+
+    fun removerConteudo(conteudo: ConteudoEducacional) {
+        if (conteudos.remove(conteudo)) {
+            println("Conteúdo ${conteudo.nome} foi removido.")
+        } else {
+            println("Conteúdo ${conteudo.nome} não foi encontrado.")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criar alguns conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val conteudo2 = ConteudoEducacional("OOP em Kotlin", 90)
+
+    // Criar uma formação
+    val formacao = Formacao("Desenvolvimento Android", Nivel.BASICO)
+
+    // Adicionar conteúdo à formação
+    formacao.adicionarConteudo(conteudo1)
+    formacao.adicionarConteudo(conteudo2)
+
+    // Criar alguns usuários
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Bob")
+
+    // Matricular usuários
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario1) // Teste de duplicação
+
+    println("Usuários inscritos: ${formacao.inscritos.map { it.nome }}")
 }
+
